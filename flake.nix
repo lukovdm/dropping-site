7f6cc865c590955332832076e1270118b2547028
Luko van der Maas <me@luko.dev>
2024-01-28 18:56:25 +0100

    initial commi

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "BF2 dropping site";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          python312
+          poetry
+          flyctl
+          openssl
+          httpie
+        ];
+      };
+    });
+}
